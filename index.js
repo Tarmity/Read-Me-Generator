8083a667a2ac1c7cfe84efaf58e5c3ae31016343
Tarmity Flassman <tarmity.flassman@gmail.com>
2020-07-16 12:33:46 +1000

    added the Questions to the index file

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 // array of questions for user
 const questions = [
-
+    "What is the title of you project?", 
+    "What is the description of you project?",
+    "Describe the installation insructions?",
+    "What is the Usage information?",
+    "What are the contribution guidelines?",
+    "What are the test instructions?",
+    "What license would you like to use?",
+    "what is your GitHub username?",
+    "What is you email address?"
 ];
 
 // function to write README file
