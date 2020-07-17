5ecc33d9cb7add4b2c274b33666a55e4e5cc46a2
Tarmity Flassman <tarmity.flassman@gmail.com>
2020-07-17 12:23:00 +1000

    updated the questions array to the correct layout also added a function testing the input

--- index.js
+++ index.js
@@ -1,15 +1,62 @@
+const inquirer = require ("inquirer");
+const fs = require ('fs');
+
 // array of questions for user
-const questions = [
-    "What is the title of you project?", 
-    "What is the description of you project?",
-    "Describe the installation insructions?",
-    "What is the Usage information?",
-    "What are the contribution guidelines?",
-    "What are the test instructions?",
-    "What license would you like to use?",
-    "what is your GitHub username?",
-    "What is you email address?"
-];
+inquirer.prompt([
+   {
+       type: "input",
+       name: "title",
+       message: "What is the title of you project?"
+   },
+   {
+      type: "input",
+      name: "description",
+      message: "What is the description of you project?",
+   },
+   {
+      type: "input",
+      name: "installation",
+      message:"Describe the installation insructions?"
+   },
+   {
+      type: "input",
+      name: "usage",
+      message:"What is the Usage information?"
+   },
+   {
+      type: "input", 
+      name: "usage",
+      message:"What are the contribution guidelines?"
+   },
+   {
+      type: "input",
+      name: "test",
+      message:"What are the test instructions?"
+   },
+   {
+      type: "input",
+      name: "license",
+      message:"What license would you like to use?"
+   },
+   {
+      type: "input",
+      name: "username",
+      message:"what is your GitHub username?"
+   },
+   {
+      type: "input",
+      name: "email",
+      message:"What is you email address?"
+   }
+]).then (function(response){
+    if (response.input === response.message){
+        console.log("Success!");
+    }
+    else{
+        console.log("Wrong");
+    }
+});
+
 
 // function to write README file
 function writeToFile(fileName, data) {
