d8dabbad7105ffbe209bbabe86d20c788da50ebc
Tarmity Flassman <tarmity.flassman@gmail.com>
2020-07-19 16:18:43 +1000

    add the content the questiond to produce the  read ME

--- README.md
+++ README.md
@@ -1,7 +1,35 @@
-# readmeGenerator
-## read
-### read
+# undefined
+  ![License](https://img.shields.io/github/license/tarmity/asdgfgfre)
+  
+  ## Description
+  * grewgwer
 
-dwawda
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#Usage)
+  * [License](#License)
+  * [Contributing](#Contributing)
+  * [Tests](#Tests)
+  * [Questions](#Questions)
+  
 
-[text to link](https://google.com)
+  ## Installation
+  To install necessary dependencies, run the following command:
+  crt
+
+  ## Usage
+  regergef 
+
+  ## License
+  * MIT
+
+  ## Contributing
+  bvrtgvert
+
+  ## Tests
+  To eliminate bugs, type the following commend to test application:
+      fdgsdgggre fegeer
+
+  ## Questions
+  * If you have any questions, feel free to contact me via email: tarmity.flassman@gmail.com
+  * My GitHub profile: https://github.com/tarmity
--- index.js
+++ index.js
@@ -1,96 +1,130 @@
-const inquirer = require ("inquirer");
-const fs = require ('fs');
-const path = require('path');
+onst inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
-inquirer.prompt([
-   {
-       type: "input",
-       name: "title",
-       message: "What is the title of you project?"
-   },
-   {
-      type: "input",
-      name: "description",
-      message: "What is the description of you project?",
-   },
-   {
-      type: "input",
-      name: "installation",
-      message:"Describe the installation insructions?"
-   },
-   {
-      type: "input",
-      name: "usage",
-      message:"What is the Usage information?"
-   },
-   {
-      type: "input", 
-      name: "usage",
-      message:"What are the contribution guidelines?"
-   },
-   {
-      type: "input",
-      name: "test",
-      message:"What are the test instructions?"
-   },
-   {
-      type: "input",
-      name: "license",
-      message:"What license would you like to use?"
-   },
-   {
-      type: "input",
-      name: "username",
-      message:"what is your GitHub username?"
-   },
-   {
-      type: "input",
-      name: "email",
-      message:"What is you email address?"
-   }
-]).then (function(response){
-    // save response to a file. 
-    console.log(response)
-    const fileName = "Read Me Generator.md";
-
-
-
-    writeToFile(fileName, response)
-    // if (response.input === response.message){
-    //     console.log("Success!");
-    // }
-    // else{
-    //     console.log("Wrong");
-    // }
-});
-
+const questions = [{
+    type: "input",
+    message: "What is your GitHub username?",
+    name: "username"
+  },
+  {
+    type: "input",
+    message: "What is your email address?",
+    name: "email"
+  },
+  {
+    type: "input",
+    message: "What is your project's name?",
+    name: "project"
+  },
+  {
+    type: "input",
+    message: "What is your repository's name?",
+    name: "repoName"
+  },
+  {
+    type: "input",
+    message: "Please write a short description of your project?",
+    name: "description"
+  },
+  {
+    type: "list",
+    message: "What kind of license should your project have?",
+    name: "licence",
+    choices: [
+        "MIT", 
+        "GPLv3", 
+        "Apache",
+        "BSD 3"
+      ] 
+  },
+  {
+    type: "input",
+    message: "What command should be run to install dependencies?",
+    name: "installDepCommand"
+  },
+  {
+    type: "input",
+    message: "What command should be run to run tests?",
+    name: "runTestCommand"
+  },{
+    type: "input",
+    message: "What does the user need to know about using the repo?",
+    name: "repoUse"
+  },
+  {
+    type: "input",
+    message: "What does the user need to know about contributing to the repo?",
+    name: "repoContribute"
+  }
+];
 
 // function to write README file
 function writeToFile(fileName, data) {
-    const title = data.title;
-    
-    const pathToSave = path.join(__dirname, fileName);
-   
-    const myAwesomeSpeech = `# ${data.title}
-
-Description
---------------------
-email: ${data.email}
---------------------
-
-email: ${data.email}
-email: ${data.email}
-email: ${data.email}
-`
-    //console.log(pathToSave);
-    fs.writeFileSync(pathToSave, myAwesomeSpeech)
-
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+          throw err;
+        }
+        console.log("Successful");
+      });
 }
 
 // function to initialize program
 function init() {
+    inquirer
+    .prompt(questions)
+    .then(function({
+      username, 
+      email, 
+      project,
+      repoName, 
+      description, 
+      licence, 
+      installDepCommand, 
+      runTestCommand, 
+      repoUse, 
+      repoContribute}) {
+        if (
+        username !== "" && 
+        email !== "" && 
+        project !=="" &&
+        repoName !=="" && 
+        description !=="" && 
+        licence !=="" && 
+        installDepCommand !=="" && 
+        runTestCommand !=="" &&
+        repoUse !=="" &&
+        repoContribute !=="") {
+            // console.log(generateMarkdown({
+            //   username, 
+            //   email, 
+            //   project,
+            //   repoName, 
+            //   description, 
+            //   licence, 
+            //   installDepCommand, 
+            //   runTestCommand, 
+            //   repoUse, 
+            //   repoContribute}));
 
+            writeToFile("README.md", generateMarkdown({
+              username, 
+              email, 
+              project,
+              repoName, 
+              description, 
+              licence, 
+              installDepCommand, 
+              runTestCommand, 
+              repoUse, 
+              repoContribute}));
+        }
+        else {
+            console.log("Please enter all information");
+            init();
+        }
+    })
 }
 
 // function call to initialize program
